3e6d951e2445dfa030c8186b626e84e65c986d6f
Ravula Swathi <84339826+ravulaSwathi21@users.noreply.github.com>
2022-10-19 20:02:00 +0530

    Update EC2Script.tf

--- EC2Script.tf
+++ EC2Script.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   region  = "ap-south-1"
-  access_key = "AKIAVQ2PP6XLIMUD3XBA"
-  secret_key = "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"
+  access_key = "AKIAVQ2PP6XLPJNQIVXG"
+  secret_key = "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"
 }
 
 resource "aws_instance" "app_server" {
-  ami = "ami-01216e7612243e0ef"
+  ami           = "ami-01216e7612243e0ef"
   instance_type = "t2.micro"
 
   tags = {
     Name = "ExampleAppServerInstance"
-}
+  }
 }
